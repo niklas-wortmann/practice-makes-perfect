f785299459b8e71b5417e8dc5961d4007c01b216
Jan-Niklas Wortmann <jwo@Jans-MacBook-Pro.local>
2019-10-21 22:08:51 +0200

    try netlify

--- docs/styles-83c274839114ee73a43c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{756:function(n,w,o){}}]);
-//# sourceMappingURL=styles-83c274839114ee73a43c.js.map
---
+++ docs/styles-d7569a2c4ace1a52d4d4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{757:function(n,w,o){}}]);
+//# sourceMappingURL=styles-d7569a2c4ace1a52d4d4.js.map
