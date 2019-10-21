20124fecbed0d12220a07708632a2442d3289cea
Jan-Niklas Wortmann <jwo@Jans-MacBook-Pro.local>
2019-10-21 20:21:29 +0200

    update docs

--- docs/styles-30eea462c02bae2ca3d7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{756:function(n,w,o){}}]);
-//# sourceMappingURL=styles-30eea462c02bae2ca3d7.js.map
--- docs/styles-e086889ce2bb12c0f8b0.js
+++ docs/styles-e086889ce2bb12c0f8b0.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{756:function(n,w,o){}}]);
-//# sourceMappingURL=styles-205d807d5bc7c4f46a6c.js.map
+//# sourceMappingURL=styles-e086889ce2bb12c0f8b0.js.map
